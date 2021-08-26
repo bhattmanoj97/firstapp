6e528d049bcc418337af63eede73967af5b91f70
bhattmanoj97 <89565299+bhattmanoj97@users.noreply.github.com>
2021-08-26 18:03:08 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $shop_domain = 'https://shopbhatt.myshopify.com';
 $token = 'e4c17832f092a9041cbba1065e9dacaa';
 $key = '08b22714d544265d95b679ec26bdbcaf';
-$secret = 'your secret';
+$secret = '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';
 
 $client = new ShopifyClient($shop_domain, $token, $api_key, $secret);
 

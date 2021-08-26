cc5c0bf78bd556a14683cb834b539036a3106521
bhattmanoj97 <89565299+bhattmanoj97@users.noreply.github.com>
2021-08-26 12:43:52 +0530

    Update install.php

--- install.php
+++ install.php
@@ -2,7 +2,7 @@
 
 // Set variables for our request
 $shop = $_GET['shop'];
-$api_key = "1r30mrvCFMfq2DLGuIXyY2veEJVgTtDD";
+$api_key = "c684692cc46bd5be2c48cf4b800fb4e6";
 $scopes = "read_orders,write_products";
 $redirect_uri = "http://localhost/generate_token.php";
 

a1e3ef8f65c3ca74c12b553da04756b17326dc7f
bhattmanoj97 <89565299+bhattmanoj97@users.noreply.github.com>
2021-08-26 13:51:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
-echo "Hello";
+foreach($collects as $collect){ 
+    foreach($collect as $key => $value){ 
+    	$products = shopify_call($token, $shop, "/admin/dd76b5b85b961b69a9277bf161232f30/2021-01/products/".$value['product_id'].".json", array(), 'GET');
+		$products = json_decode($products['response'], JSON_PRETTY_PRINT);
+		echo $products['product']['title'];
+    }
+}
 ?>

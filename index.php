ea9597f3e6ff17f52e454a18f088a972ccda9dc6
bhattmanoj97 <89565299+bhattmanoj97@users.noreply.github.com>
2021-08-26 18:00:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,10 @@
 <?php
-echo "yesssss";
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+$shop_domain = 'https://shopbhatt.myshopify.com';
+$token = 'e4c17832f092a9041cbba1065e9dacaa';
+$key = '08b22714d544265d95b679ec26bdbcaf';
+$secret = 'your secret';
 
-require_once("inc/functions.php");
+$client = new ShopifyClient($shop_domain, $token, $api_key, $secret);
 
-
-$token ="e4c17832f092a9041cbba1065e9dacaa";
-$shop = "shopbhatt";
-
-$collectionList = shopify_call($token, $shop, "/admin/api/2020-07/custom_collections.json", array(), 'GET');
-$collectionList = json_decode($collectionList['response'], JSON_PRETTY_PRINT);
-$collection_id = $collectionList['custom_collections'][0]['id'];
-
-$array = array("collection_id"=>$collection_id);
-$collects = shopify_call($token, $shop, "/admin/api/2020-07/collects.json", $array, 'GET');
-$collects = json_decode($collects['response'], JSON_PRETTY_PRINT);
-
-foreach($collects as $collect){
-    foreach($collect as $key => $value){
-    	$products = shopify_call($token, $shop, "/admin/api/2019-07/products/".$value['product_id'].".json", array(), 'GET');
-		$products = json_decode($products['response'], JSON_PRETTY_PRINT);
-		echo $products['product']['title'];
-    }
-}
+print_r($client);
 ?>

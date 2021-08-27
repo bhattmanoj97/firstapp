7e6c6401ad51b684184b23298d196e71f2f1939f
bhattmanoj97 <89565299+bhattmanoj97@users.noreply.github.com>
2021-08-27 09:30:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,8 @@ $url = "https://08b22714d544265d95b679ec26bdbcaf:shppa_6f3a48f6c5757c0fc37e9c7ba
 $ch = curl_init();   
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);   
 curl_setopt($ch, CURLOPT_URL, $url);   
-$res = curl_exec($ch);   
-echo $res;   
+$res = curl_exec($ch);      
+<tr> 
+ <td> echo $res; </td>
+</tr>
 ?>  

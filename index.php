4d3433a3240835a7fe4fddd472dea84c5b19191a
bhattmanoj97 <89565299+bhattmanoj97@users.noreply.github.com>
2021-08-30 15:24:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,5 @@ $hmac = $_GET['hmac'];
 $serializeArray = serialize($requests);
 $requests = array_diff_key($requests, array('hmac' => ''));
 ksort($requests);
-echo $shop;
-
+print_r($requests);
 ?>

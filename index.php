c3d4806a9c988ffd35e2236883bcb0efda865286
bhattmanoj97 <89565299+bhattmanoj97@users.noreply.github.com>
2021-08-30 15:31:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ $hmac = $_GET['hmac'];
 $serializeArray = serialize($requests);
 $requests = array_diff_key($requests, array('hmac' => ''));
 ksort($requests);
-
-$token = "";
+echo $shop;
+$token = "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";
 $shop = "shopbhatt";
 
 $collectionList = shopify_call($token, $shop, "/admin/api/2020-07/custom_collections.json", array(), 'GET');

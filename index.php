baff1170dd8f239b720554e6a8c021cff928980d
bhattmanoj97 <89565299+bhattmanoj97@users.noreply.github.com>
2021-08-30 15:46:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -10,12 +10,26 @@ ksort($requests);
 $token = "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";
 $shop = "shopbhatt";
 
-$collectionList = shopify_call($token, $shop, "/admin/api/2020-07/custom_collections.json", array(), 'GET');
-$collectionList = json_decode($collectionList['response'], JSON_PRETTY_PRINT);
-$collection_id = $collectionList['custom_collections'][0]['id'];
+// $collectionList = shopify_call($token, $shop, "/admin/api/2020-07/custom_collections.json", array(), 'GET');
+// $collectionList = json_decode($collectionList['response'], JSON_PRETTY_PRINT);
+// $collection_id = $collectionList['custom_collections'][0]['id'];
 
+// $array = array("collection_id"=>$collection_id);
+// $collects = shopify_call($token, $shop, "/admin/api/2020-07/products.json", $array, 'GET');
+// $collects = json_decode($collects['response'], JSON_PRETTY_PRINT);
+// print_r($collects);
+
+$collection_id = 276995047597;
 $array = array("collection_id"=>$collection_id);
-$collects = shopify_call($token, $shop, "/admin/api/2020-07/products.json", $array, 'GET');
+$collects = shopify_call($token, $shop, "/admin/api/2020-07/collects.json", $array, 'GET');
 $collects = json_decode($collects['response'], JSON_PRETTY_PRINT);
-print_r($collects);
+
+foreach($collects as $collect){
+    foreach($collect as $key => $value){
+    	$products = shopify_call($token, $shop, "/admin/api/2019-07/products/".$value['product_id'].".json", array(), 'GET');
+		$products = json_decode($products['response'], JSON_PRETTY_PRINT);
+		echo $products['product']['title'];
+    }
+}
+
 ?>

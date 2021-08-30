7839910f2a2c56d41630a917a82c9d3b2d0df6b6
bhattmanoj97 <89565299+bhattmanoj97@users.noreply.github.com>
2021-08-30 14:53:52 +0530

    Update install.php

--- install.php
+++ install.php
@@ -4,7 +4,7 @@
 $shop = $_GET['shop'];
 $api_key = "c684692cc46bd5be2c48cf4b800fb4e6";
 $scopes = "read_orders,write_orders,read_products,write_products";
-$redirect_uri = "http://localhost/generate_token.php";
+$redirect_uri = "https://manojfirst1.herokuapp.com/generate_token.php";
 
 // Build install/approval URL to redirect to
 $install_url = "https://" . $shop . ".myshopify.com/admin/oauth/authorize?client_id=" . $api_key . "&scope=" . $scopes . "&redirect_uri=" . urlencode($redirect_uri);

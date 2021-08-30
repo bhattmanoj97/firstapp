a69474b87880fe830c469231eba815cc1e11efc3
bhattmanoj97 <89565299+bhattmanoj97@users.noreply.github.com>
2021-08-30 14:56:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php   
-$url = "https://08b22714d544265d95b679ec26bdbcaf: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@shopbhatt.myshopify.com/admin/api/2021-07/products.json";
+/*$url = "https://08b22714d544265d95b679ec26bdbcaf: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@shopbhatt.myshopify.com/admin/api/2021-07/products.json";
 $ch = curl_init();   
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);   
 curl_setopt($ch, CURLOPT_URL, $url);   
 $res = curl_exec($ch);    
-echo $res;  
+echo $res;  */
+echo "hello World";
  ?>  

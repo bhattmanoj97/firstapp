e921e0efe0b2933f52f67f902fb19f6421b3f399
bhattmanoj97 <89565299+bhattmanoj97@users.noreply.github.com>
2021-09-01 17:13:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -66,6 +66,8 @@ $images = json_decode($images['response'], JSON_PRETTY_PRINT);
 $item_default_image = $images['images'][0]['src'];
 //print_r($item_default_image);
 // echo '<img src="'.$item_default_image.'" style="width: 200px; height: 230px;"/>';
+echo "<img src="//cdn.shopify.com/s/files/1/0222/9076/t/10/assets/logo.png?796" alt="Logo">";
+
 ?>
          <tr>
           <td> <?php echo $key ?> </td>

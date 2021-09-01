c7843ab4948b7457d7bb700dbb3c616e168a895b
bhattmanoj97 <89565299+bhattmanoj97@users.noreply.github.com>
2021-09-01 13:27:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,23 @@ $collection_id = 276995047597;
 $array = array("collection_id"=>$collection_id);
 $collects = shopify_call($token, $shop, "/admin/api/2020-07/collects.json", $array, 'GET');
 $collects = json_decode($collects['response'], JSON_PRETTY_PRINT); ?>
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
 
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
 <table>
 	<tr>
 	  <th>Product Id </th>

5e9e2a3c7f4c02a03740ed601af7d63a5f03ae44
bhattmanoj97 <89565299+bhattmanoj97@users.noreply.github.com>
2021-09-01 15:32:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -56,17 +56,9 @@ $variants = shopify_call($token, $shop, "/admin/api/2020-10/products/".$value['p
 $variants = json_decode($variants['response'], JSON_PRETTY_PRINT);
 $originalPrice = $variants['variants'][0]['compare_at_price'];
 $discountedPrice = $variants['variants'][0]['price'];
-         <tr>
-	  <td> <?php echo $products['product']['id'];    ?> </td>
-	  <td> <?php echo $products['product']['title']; ?> </td>
-          <td> <?php echo $discountedPrice ?> </td>
-          <td> <?php echo $products['product']['vendor']; ?> </td>
-          </tr>
+echo $products['product']['title'] . " | Now at $" . $discountedPrice . " | Before: <del>$" . $originalPrice . "</del><br />";
+}
+} 
 
-    <?php }
-} ?>
-</table>
-
-<?php 
 
 ?>

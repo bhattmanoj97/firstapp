4635817f7f02238bfe7635f491ed575d59622fbb
bhattmanoj97 <89565299+bhattmanoj97@users.noreply.github.com>
2021-09-01 18:05:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -64,16 +64,15 @@ $discountedPrice = $variants['variants'][0]['price'];
 $images = shopify_call($token, $shop, "/admin/api/2020-10/products/".$products['product']['id']."/images.json", array(), 'GET');
 $images = json_decode($images['response'], JSON_PRETTY_PRINT);
 $item_default_image = $images['images'][0]['src'];
-$item_default_image = $images['images'][1]['src'];
+
 
 // echo '<img src="'.$item_default_image.'" style="width: 200px; height: 230px;"/>';
 
-print_r($item_default_image);
-// if($images == null){
-// echo $item_default_image
-// }else{
-// echo "no image";
-// }
+foreach($images['images'] as $image){
+foreach($image as $key => $value){
+echo '<img src="'.$value['src'].'" />';
+}
+}
 ?>
 
          <tr>

ae325e2c6d6e7d52349bbe6676b9e93f4c1c706a
bhattmanoj97 <89565299+bhattmanoj97@users.noreply.github.com>
2021-09-02 15:57:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -10,14 +10,13 @@ ksort($requests);
 $token = "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";
 $shop = "shopbhatt";
 
-$collectionList = shopify_call($token, $shop, "/admin/api/2020-07/custom_collections.json", array(), 'GET');
-$collectionList = json_decode($collectionList['response'], JSON_PRETTY_PRINT);
-$collection_id = $collectionList['custom_collections'][0]['id'];
+// $collectionList = shopify_call($token, $shop, "/admin/api/2020-07/custom_collections.json", array(), 'GET');
+// $collectionList = json_decode($collectionList['response'], JSON_PRETTY_PRINT);
+// $collection_id = $collectionList['custom_collections'][0]['id'];
 
-$array = array("collection_id"=>$collection_id);
-$collects = shopify_call($token, $shop, "/admin/api/2020-07/products.json", $array, 'GET');
-$collects = json_decode($collects['response'], JSON_PRETTY_PRINT);
-print_r($collects); 
+// $array = array("collection_id"=>$collection_id);
+// $collects = shopify_call($token, $shop, "/admin/api/2020-07/products.json", $array, 'GET');
+// $collects = json_decode($collects['response'], JSON_PRETTY_PRINT);
 
 $collection_id = 276995047597;
 $array = array("collection_id"=>$collection_id);
@@ -56,11 +55,11 @@ foreach($collects as $collect){
 foreach($collect as $key => $value){
 $products = shopify_call($token, $shop, "/admin/api/2020-10/products/".$value['product_id'].".json", array(), 'GET');
 $products = json_decode($products['response'], JSON_PRETTY_PRINT);
-//images
-//$images = shopify_call($token, $shop, "/admin/api/2020-10/products/".$products['product']['id']."/images.json", array(), 'GET');
-//$images = json_decode($images['response'], JSON_PRETTY_PRINT);
 
- $item_default_image = $products['images'][0]['src'][0];
+//images
+$images = shopify_call($token, $shop, "/admin/api/2020-10/products/".$products['product']['id']."/images.json", array(), 'GET');
+$images = json_decode($images['response'], JSON_PRETTY_PRINT);
+$item_default_image = $products['images'][0]['src'];
 
 ?>
 

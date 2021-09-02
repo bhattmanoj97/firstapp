b1b421d6e826d8d41ac57819c81756ba4ccc75f6
bhattmanoj97 <89565299+bhattmanoj97@users.noreply.github.com>
2021-09-02 17:38:22 +0530

    Update index.php

--- index.php
+++ index.php
@@ -57,9 +57,8 @@ $products = shopify_call($token, $shop, "/admin/api/2020-10/products/".$value['p
 $products = json_decode($products['response'], JSON_PRETTY_PRINT);
 
 //images
-$images = shopify_call($token, $shop, "/admin/api/2020-01/products/".$value['product']['id']."/images.json", array(), 'GET');
-print_r($images);
-//$images = shopify_call($token, $shop, "/admin/api/2020-01/products/".$products['product']['id']."/images.json", array(), 'GET');
+// print_r($images);
+$images = shopify_call($token, $shop, "/admin/api/2020-01/products/".$products['product']['id']."/images.json", array(), 'GET');
 $images = json_decode($images['response'], JSON_PRETTY_PRINT);
 $item_default_image = $images['images'][0]['src'];
 print_r($item_default_image);

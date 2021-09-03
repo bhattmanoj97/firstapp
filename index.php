67621db7761ae01287c434842af0fb7316d04b2a
bhattmanoj97 <89565299+bhattmanoj97@users.noreply.github.com>
2021-09-03 11:49:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -71,7 +71,9 @@ $item_default_image = $images['images'][0]['src'];
          <tr>
           <td> <?php echo $key ?> </td>
           <td> <?php echo $products['product']['id'];    ?> </td>
-          <td> <?php echo '<img src="'.$item_default_image.'" style="width:50px; height:50px;"/>'; ?></td>
+          <td> <?php echo <a href="'.$storeURL.'/products/'.$products['product']['handle'].'" target="_blank">
+             <img src="'.$item_default_image.'" style="width: 50px; height: 50px;"/>
+            </a> ?></td>
           <td> <?php echo $products['product']['title']; ?> </td>
           <td> <?php echo $products['product']['variants'][0]['price']; ?> </td>
           <td> <?php echo $originalPrice ?> </td>
